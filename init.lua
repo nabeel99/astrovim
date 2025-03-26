ac4945ddac16ff2812c221f0c4596a169847cb8b
aaran <nabeelnaveed98@icloud.com>
2025-03-27 04:43:29 +0500

    add init lua

--- init.lua
+++ init.lua
@@ -17,3 +17,16 @@ end
 
 require "lazy_setup"
 require "polish"
+require "mappings.rust"
+vim.keymap.set("n", "<Up>", "gk", { noremap = true })
+vim.keymap.set("n", "<Down>", "gj", { noremap = true })
+-- vim.keymap.set('i', '<Up>', 'gk', { noremap = true })
+-- vim.keymap.set('i', '<Down>', 'gj', { noremap = true })
+vim.keymap.set("v", "<Up>", "gk", { noremap = true })
+vim.keymap.set("v", "<Down>", "gj", { noremap = true })
+
+-- This will remove any active search highlight until you initiate a new search.
+-- If you want to prevent the highlight from being persistent after each search, add this to your init.lua or init.vim:
+-- TODO: Cross-Check if this is necessary: Saturday 5 October 2024
+--
+vim.opt.hlsearch = false
