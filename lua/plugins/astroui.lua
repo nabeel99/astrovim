eaf9be2617ce4bb47a946aaea560057335a23d6c
aaran <nabeelnaveed98@icloud.com>
2025-03-27 06:16:20 +0500

    chore.

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -5,69 +5,69 @@
 -- --       as this provides autocomplete and documentation while editing
 --
 -- ---@type LazySpec
--- return {
---   "AstroNvim/astroui",
---   ---@type AstroUIOpts
---   opts = {
---     -- change colorscheme
---     colorscheme = "astrodark",
---     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
---     highlights = {
---       init = { -- this table overrides highlights in all themes
---         -- Normal = { bg = "#000000" },
---       },
---       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
---         -- Normal = { bg = "#000000" },
---       },
---     },
---     -- Icons can be configured throughout the interface
---     icons = {
---       -- configure the loading of the lsp in the status line
---       LSPLoading1 = "⠋",
---       LSPLoading2 = "⠙",
---       LSPLoading3 = "⠹",
---       LSPLoading4 = "⠸",
---       LSPLoading5 = "⠼",
---       LSPLoading6 = "⠴",
---       LSPLoading7 = "⠦",
---       LSPLoading8 = "⠧",
---       LSPLoading9 = "⠇",
---       LSPLoading10 = "⠏",
---     },
---   },
--- }
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
+    -- change colorscheme
+    colorscheme = "astrodark",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
-      -- set highlights for all themes
-      -- use a function override to let us use lua to retrieve
-      -- colors from highlight group there is no default table
-      -- so we don't need to put a parameter for this function
-      init = function()
-        local get_hlgroup = require("astroui").get_hlgroup
-        -- get highlights from highlight groups
-        local bg = get_hlgroup("Normal").bg
-        local bg_alt = get_hlgroup("Visual").bg
-        local green = get_hlgroup("String").fg
-        local red = get_hlgroup("Error").fg
-        -- return a table of highlights for snacks.picker based on
-        -- colors retrieved from highlight groups
-        return {
-          SnacksPickerBorder = { fg = bg_alt, bg = bg },
-          SnacksPicker = { bg = bg },
-          SnacksPickerPreviewBorder = { fg = bg, bg = bg },
-          SnacksPickerPreview = { bg = bg },
-          SnacksPickerPreviewTitle = { fg = bg, bg = green },
-          SnacksPickerBoxBorder = { fg = bg, bg = bg },
-          SnacksPickerInputBorder = { fg = bg, bg = bg },
-          SnacksPickerInputSearch = { fg = red, bg = bg },
-          SnacksPickerListBorder = { fg = bg, bg = bg },
-          SnacksPickerList = { bg = bg },
-          SnacksPickerListTitle = { fg = bg, bg = bg },
-        }
-      end,
+      init = { -- this table overrides highlights in all themes
+        -- Normal = { bg = "#000000" },
+      },
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+        -- Normal = { bg = "#000000" },
+      },
+    },
+    -- Icons can be configured throughout the interface
+    icons = {
+      -- configure the loading of the lsp in the status line
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
     },
   },
 }
+-- return {
+--   "AstroNvim/astroui",
+--   ---@type AstroUIOpts
+--   opts = {
+--     highlights = {
+--       -- set highlights for all themes
+--       -- use a function override to let us use lua to retrieve
+--       -- colors from highlight group there is no default table
+--       -- so we don't need to put a parameter for this function
+--       init = function()
+--         local get_hlgroup = require("astroui").get_hlgroup
+--         -- get highlights from highlight groups
+--         local bg = get_hlgroup("Normal").bg
+--         local bg_alt = get_hlgroup("Visual").bg
+--         local green = get_hlgroup("String").fg
+--         local red = get_hlgroup("Error").fg
+--         -- return a table of highlights for snacks.picker based on
+--         -- colors retrieved from highlight groups
+--         return {
+--           SnacksPickerBorder = { fg = bg_alt, bg = bg },
+--           SnacksPicker = { bg = bg },
+--           SnacksPickerPreviewBorder = { fg = bg, bg = bg },
+--           SnacksPickerPreview = { bg = bg },
+--           SnacksPickerPreviewTitle = { fg = bg, bg = green },
+--           SnacksPickerBoxBorder = { fg = bg, bg = bg },
+--           SnacksPickerInputBorder = { fg = bg, bg = bg },
+--           SnacksPickerInputSearch = { fg = red, bg = bg },
+--           SnacksPickerListBorder = { fg = bg, bg = bg },
+--           SnacksPickerList = { bg = bg },
+--           SnacksPickerListTitle = { fg = bg, bg = bg },
+--         }
+--       end,
+--     },
+--   },
+-- }
