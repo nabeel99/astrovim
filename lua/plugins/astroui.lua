12db385270e213363b1b492fa769a9639188b80b
aaran <nabeelnaveed98@icloud.com>
2025-03-27 18:06:33 +0500

    add support for system theme, disable astroui and gh plugins

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,3 +1,5 @@
+ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true return {}
 --
 -- -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- -- Configuration documentation can be found with `:h astroui`
